8c6ec28dcf72e9d28b7ce1b82733021a9dd207e6
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2017-02-07 14:05:40 +0700

    Add project files.

---
+++ Modul1_ASP_MVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Modul1_ASP_MVC.Security;
+using System.Web.Mvc;
+
+namespace Modul1_ASP_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
